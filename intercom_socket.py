a165328bc251643466bf7ec022d7060c8d2211d4
Gionny <wowgiona49@gmail.com>
2020-09-13 14:31:21 +0200

    added socketless version

--- intercom_socket.py
+++ intercom_socket.py
@@ -1,5 +1,7 @@
 import sys
 import pyaudio
+import socket
+import select
 import math
 import requests
 import struct
@@ -19,6 +21,10 @@ SLACK_WEBHOOK = environ['SLACK_WEB_HOOK']
 TELEGRAM_BOT_TOKEN = environ['TELEGRAM_INTERCOM_TOKEN']
 
 audio = pyaudio.PyAudio()
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('', 4444))
+serversocket.listen(5)
 
 def telegram_message(message='test'):
     r = requests.get('https://api.telegram.org/bot{0}/sendMessage?chat_id=-1001460035878&text={1}'.format(TELEGRAM_BOT_TOKEN, message))
@@ -56,25 +62,43 @@ def get_rms( block ):
 
 
 def callback(in_data, frame_count, time_info, status):
-    amplitude = get_rms( in_data )
-    if amplitude >= INTERCOM_THRESHOLD:
-        print("amplitude -> ", str(amplitude))
-        notification_manager('CORRI CITOFONO!!!')
-        sleep(5)
+    for s in read_list[1:]:
+        amplitude = get_rms( in_data )
+        if amplitude >= INTERCOM_THRESHOLD:
+            print("amplitude -> ", str(amplitude))
+            notification_manager('CORRI CITOFONO!!!')
+            sleep(5)
+        try:
+            s.send(in_data)
+        except:
+            pass
 
     return (None, pyaudio.paContinue)
 
 stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK, stream_callback=callback)
 
+read_list = [serversocket]
 print("recording...")
-
 
 try:
     while True:
-        pass
+        readable, writable, errored = select.select(read_list, [], [])
+        for s in readable:
+            if s is serversocket:
+                (clientsocket, address) = serversocket.accept()
+                read_list.append(clientsocket)
+                print("Connection from ", address)
+            else:
+                try:
+                    data = s.recv(1024)
+                except:
+                    read_list.remove(s)
 except KeyboardInterrupt:
+    pass
 
-    print("finished recording")
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+print("finished recording")
+
+serversocket.close()
+stream.stop_stream()
+stream.close()
+audio.terminate()
